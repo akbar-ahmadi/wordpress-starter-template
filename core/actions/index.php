df219f81bdcfe7561bc34a25f2b5b33b4c0e54f8
Akbar Ahmadi Saray <76680218+akbar-ahmadi@users.noreply.github.com>
2021-01-12 19:54:19 +0330

    Update index.php

--- core/actions/index.php
+++ core/actions/index.php
@@ -3,3 +3,4 @@ require_once __DIR__.'enqueue/index.php';
 require_once __DIR__.'post-type/index.php';
 require_once __DIR__.'taxanomy/index.php';
 require_once __DIR__.'menu/index.php';
+// asdasdasdasda
